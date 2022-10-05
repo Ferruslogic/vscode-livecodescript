28258cab1184c2be45e5a20394b2bad726427fc2
Ruaridh Bell <107618943+livecoderuaridh@users.noreply.github.com>
2022-10-05 18:12:02 +0100

    [[live-server]] Adds toast notifications for users (#19)
    This patch adds toast notifications to indicate to the user what is 
    happening in the connection between vscode and LiveCode. Also tidied up 
    some functions.

--- src/features/livecodescript/LCSserverProvider.ts
+++ src/features/livecodescript/LCSserverProvider.ts
@@ -2,28 +2,21 @@ import * as vscode from "vscode";
 import { TextDecoder } from "text-encoding";
 import { PromiseSocket } from "promise-socket";
 
-const enum Setting {
-	Enable = "livecodescript.server.enable",
-	Host = "livecodescript.server.host",
-	Port = "livecodescript.server.port",
-}
-
 export default class LivecodescriptServerProvider {
 	private enabled: boolean;
 	private host: string;
 	private port: number;
 
-	constructor() {}
+	constructor() {
+		this.loadConfiguration();
+	}
 
 	public activate(subscriptions: vscode.Disposable[]) {
 		subscriptions.push(this);
 		subscriptions.push(
 			vscode.workspace.onDidSaveTextDocument(
 				async ({ fileName, languageId, lineAt }) => {
-					if (this.enabled === undefined) {
-						await this.loadConfiguration();
-					}
-
+					console.log("DOCUMENT SAVED");
 					if (this.enabled && languageId === "livecodescript") {
 						const regex = '"([-.:a-zA-Z0-9_s?!]+)"';
 						const scriptName = lineAt(0).text.match(regex)[1];
@@ -33,77 +26,78 @@ export default class LivecodescriptServerProvider {
 							filename: fileName,
 						};
 
-						let prms = new URLSearchParams(query);
-
-						const socket = new PromiseSocket();
-						socket.setTimeout(300);
-
-						try {
-							await socket.connect(this.port, this.host);
-							await socket.write(`${prms.toString()}\n`);
-
-							const data = await socket.read();
-							let result = new TextDecoder().decode(data);
-
-							if (result !== "success") {
-								if (result.includes("error: 360")) {
-									console.log(
-										"error: script is being executed within Livecode"
-									);
-								} else {
-									console.log(
-										`error running command in LiveCode: ${result}`
-									);
-								}
-							} else {
-								console.log(
-									"command successfully sent to LiveCode"
-								);
-							}
-							socket.destroy();
-						} catch (err) {
-							console.log(err);
-							socket.destroy();
-						}
+						this.sendToLiveCode(query);
 					}
 				}
+			)
+		);
+		subscriptions.push(
+			vscode.workspace.onDidChangeConfiguration(() =>
+				this.loadConfiguration()
 			)
 		);
 	}
 
 	public dispose(): void {}
 
-	private async loadConfiguration(): Promise<void> {
-		const section = vscode.workspace.getConfiguration();
-		this.enabled = this.getSettingValue<boolean>(
-			section,
-			Setting.Enable,
-			false
-		);
-		this.host = this.getSettingValue<string>(
-			section,
-			Setting.Host,
-			"localhost"
-		);
-		this.port = this.getSettingValue<number>(section, Setting.Port, 61373);
+	private loadConfiguration() {
+		console.log("CONFIG CHANGED");
+		const section = vscode.workspace.getConfiguration("livecodescript");
+		this.enabled = section.get("server.enable", false);
+		this.host = section.get("server.host", "localhost");
+		this.port = section.get("server.port", 61373);
 	}
 
-	private getSettingValue<T>(
-		section: vscode.WorkspaceConfiguration,
-		setting: Setting,
-		def: T
-	): T {
-		const value = section.inspect<T>(setting);
-		if (value === undefined) {
-			return def;
-		} else if (value.workspaceValue !== undefined) {
-			return value.workspaceValue;
-		} else if (value.globalValue !== undefined) {
-			return value.globalValue;
-		} else if (value.defaultValue !== undefined) {
-			return value.defaultValue;
-		} else {
-			return def;
+	private async sendToLiveCode(query) {
+		let prms = new URLSearchParams(query);
+		const socket = new PromiseSocket();
+		socket.setTimeout(300);
+
+		try {
+			await socket.connect(this.port, this.host);
+			await socket.write(`${prms.toString()}\n`);
+
+			const data = await socket.read();
+			socket.destroy();
+			let result = new TextDecoder().decode(data);
+
+			if (result !== "success") {
+				if (result.includes("error: 360")) {
+					vscode.window.showInformationMessage(
+						"error: script is being executed within Livecode"
+					);
+				} else {
+					vscode.window
+						.showErrorMessage(
+							`error running command in LiveCode: ${result}`,
+							...["Retry"]
+						)
+						.then((selection) => {
+							console.log(selection);
+							if (selection === "Retry") {
+								this.sendToLiveCode(query);
+							}
+						});
+				}
+			} else {
+				vscode.window.showInformationMessage(
+					"command successfully sent to LiveCode"
+				);
+			}
+		} catch (err) {
+			socket.destroy();
+			console.log(err);
+			vscode.window
+				.showErrorMessage(
+					"An error occurred while updating LiveCode.",
+					...["Retry"]
+				)
+				.then((selection) => {
+					console.log(selection);
+					if (selection === "Retry") {
+						this.sendToLiveCode(query);
+					}
+				});
 		}
 	}
 }
