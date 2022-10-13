6913084d440af2f8c353fc8959dce6fd4d356808
livecoderuaridh <ruaridh.bell@livecode.com>
2022-10-13 14:34:46 +0100

    [[custom-editor]] Added custom editor support for .livecode files
    This patch adds a custom editor for .livecode files that allows you to see 
    a snapshot of the stack that has been clicked. Opening a stack in vscode 
    also now brings that stack to the front in the livecode IDE.

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,41 @@
 'use strict';
 import * as vscode from 'vscode';
 //import { DocumentSymbol } from 'vscode';
-import * as path from 'path';
-import * as nls from 'vscode-nls';
 
-import LivecodescriptValidationProvider from './features/livecodescript/LCSvalidationProvider';
+import LivecodescriptValidationProvider from "./features/livecodescript/LCSvalidationProvider";
 import { LivecodescriptFormattingProvider } from "./features/livecodescript/LCSformat";
 import { LivecodescriptDefinitionProvider } from "./features/livecodescript/LCSdefinitionProvider";
 import { livecodescriptConfigDocumentSymbolProvider } from "./features/livecodescript/LCSsymbolProvider";
 import LivecodescriptServerProvider from "./features/livecodescript/LCSserverProvider";
+import { LiveCodeStackEditorProvider } from "./features/livecodescript/LCSeditor";
+import LivecodescriptSender from "./features/livecodescript/LCSsendToLiveCode";
 
 import LivecodebuilderValidationProvider from "./features/livecodebuilder/LCBvalidationProvider";
 import { LivecodebuilderFormattingProvider } from "./features/livecodebuilder/LCBformat";
 import { LivecodebuilderDefinitionProvider } from "./features/livecodebuilder/LCBdefinitionProvider";
 import { livecodebuilderConfigDocumentSymbolProvider } from "./features/livecodebuilder/LCBsymbolProvider";
 
-
 export function activate(context: vscode.ExtensionContext) {
     
     let validator = new LivecodescriptValidationProvider();
     let LCBuilderValidator = new LivecodebuilderValidationProvider();
 
-    let server = new LivecodescriptServerProvider();
+    let sender = new LivecodescriptSender();
+    let server = new LivecodescriptServerProvider(sender);
 
     let formatProvider = new LivecodescriptFormattingProvider();
     let definitionProvider = new LivecodescriptDefinitionProvider();
     let symbolProvider = new livecodescriptConfigDocumentSymbolProvider();
 
     
-   let LCBuilderFormatProvider = new LivecodebuilderFormattingProvider();
+    let LCBuilderFormatProvider = new LivecodebuilderFormattingProvider();
     let LCBuilderDefinitionProvider = new LivecodebuilderDefinitionProvider();
     let LCBsymbolProvider = new livecodebuilderConfigDocumentSymbolProvider();
 
     LCBuilderValidator.activate(context.subscriptions);
     validator.activate(context.subscriptions);
     server.activate(context.subscriptions);
+    sender.activate(context.subscriptions);
 
 
     context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider({ scheme: "file", language: "livecodescript" }, symbolProvider));
@@ -45,5 +46,5 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider({ scheme: "file", language: "livecodebuilder" }, LCBsymbolProvider));
     context.subscriptions.push(vscode.languages.registerDefinitionProvider({ scheme: "file", language: "livecodebuilder" }, LCBuilderDefinitionProvider));
     context.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider({ scheme: "file", language: "livecodebuilder" }, LCBuilderFormatProvider));
-
+    context.subscriptions.push(LiveCodeStackEditorProvider.register(context, sender));
 }
---
+++ src/features/livecodescript/LCSsendToLiveCode.ts
@@ -0,0 +1,110 @@
+import * as vscode from "vscode";
+import { TextDecoder } from "text-encoding";
+import { PromiseSocket } from "promise-socket";
+
+export default class LivecodescriptSender {
+	private host: string;
+	private port: number;
+	private hideError: boolean = false;
+
+	constructor() {
+		this.loadConfiguration();
+	}
+
+	public dispose(): void {}
+
+	public async send(query: any) {
+		let prms = new URLSearchParams(query);
+		const socket = new PromiseSocket();
+		socket.setTimeout(300);
+
+		try {
+			await socket.connect(this.port, this.host);
+			await socket.write(`${prms.toString()}\n`);
+
+			const data = await socket.read();
+			socket.destroy();
+			let result = new TextDecoder().decode(data);
+
+			if (result !== "success") {
+				if (result.includes("error: 360")) {
+					vscode.window
+						.showErrorMessage(
+							"Error: Script is being executed within Livecode",
+							...["Retry"]
+						)
+						.then((selection) => {
+							console.log(selection);
+							if (selection === "Retry") {
+								this.send(query);
+							}
+						});
+				} else {
+					vscode.window
+						.showErrorMessage(
+							`Error running command in LiveCode: ${result}`,
+							...["Retry"]
+						)
+						.then((selection) => {
+							console.log(selection);
+							if (selection === "Retry") {
+								this.send(query);
+							}
+						});
+				}
+			} else {
+				vscode.window.showInformationMessage(
+					"LiveCode updated successfully"
+				);
+			}
+		} catch (err) {
+			socket.destroy();
+			console.log(err);
+			if (err.code === "ECONNREFUSED") {
+				if (!this.hideError) {
+					vscode.window
+						.showErrorMessage(
+							"Could not connect to LiveCode. Make sure LiveCode is running and listening on the correct port",
+							...["Retry", "Don't show again"]
+						)
+						.then((selection) => {
+							console.log(selection);
+							if (selection === "Retry") {
+								this.send(query);
+							} else if (selection === "Don't show again") {
+								this.hideError = true;
+							}
+						});
+				}
+			} else {
+				vscode.window
+					.showErrorMessage(
+						"An error occurred while updating LiveCode.",
+						...["Retry"]
+					)
+					.then((selection) => {
+						console.log(selection);
+						if (selection === "Retry") {
+							this.send(query);
+						}
+					});
+			}
+		}
+	}
+
+	public activate(subscriptions: vscode.Disposable[]) {
+		subscriptions.push(this);
+		subscriptions.push(
+			vscode.workspace.onDidChangeConfiguration(() =>
+				this.loadConfiguration()
+			)
+		);
+	}
+
+	private loadConfiguration() {
+		console.log("CONFIG CHANGED");
+		const section = vscode.workspace.getConfiguration("livecodescript");
+		this.host = section.get("server.host", "localhost");
+		this.port = section.get("server.port", 61373);
+	}
+}
